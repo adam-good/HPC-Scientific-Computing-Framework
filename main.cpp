28d7dd70fd3d8290288a163e59816c924802b77f
Adam Good <dev.adamgood@gmail.com>
2019-11-08 18:07:13 -0600

    Added basic indexing functionality

--- main.cpp
+++ main.cpp
@@ -4,17 +4,29 @@
 
 using namespace std;
 
+
 int main(void)
 {
     const unsigned int N = 3;
-    array<int, N> shape = {2,2,3};
-    vector<double> values = {1,2,3,4,5,6,7,8,9,10,11,12};
+    array<int, N> shape = {2,2,2};
+    vector<double> values = {1,2,3,4,5,6,7,8};
     
     HyperMatrix<N> M = HyperMatrix<N>(shape, values);
     HyperMatrix<N> M_0 = HyperMatrix<N>::Zeros(shape);
+    HyperMatrix<N> M_1 = HyperMatrix<N>::Ones(shape);
 
     cout << M.GetDims() << endl;
     cout << M << endl;
+    cout << M_0 << endl;
+    cout << M_1 << endl;
+    cout << M.At({0,0,0}) << endl; // 1
+    cout << M.At({1,0,0}) << endl; // 2
+    cout << M.At({0,1,0}) << endl; // 3
+    cout << M.At({1,1,0}) << endl; // 4
+    cout << M.At({0,0,1}) << endl; // 5
+    cout << M.At({1,0,1}) << endl; // 6
+    cout << M.At({0,1,1}) << endl; // 7
+    cout << M.At({1,1,1}) << endl; // 8
 
     return 0;
 }
--- matrices.h
+++ matrices.h
@@ -10,10 +10,13 @@ template<unsigned int N>
 class HyperMatrix
 {
 private:
-    std::array<int, N> shape;
     int dims = N;
+    std::array<int, N> shape;
+    std::array<int, N> strides;
     std::vector<double> values;
 
+    void calculateStride();
+    int convertIndex(std::array<int, N> indices);
     std::string toString() const;
 
 public:
@@ -28,10 +31,24 @@ public:
 
     ~HyperMatrix();
 
-    /// <summary> Creates an N dimensional array of 0s with shape `shape` </summary>
-    /// <param name=shape> Array containing the sizes of each dimenstion of the Martix</param>
+    /// <summary> Creates an N dimensional Matrix of 0s with shape `shape` </summary>
+    /// <param name=shape> Array containing the sizes of each dimension of the Martix</param>
     static HyperMatrix<N> Zeros(std::array<int, N> shape);
+
+    /// <summary> Creates an N dimensional Matrix of 0s with shape `shape` </summary>
+    /// <param name=shape> Array containing the sizes of each dimension of the Matrix </param>
+    static HyperMatrix<N> Ones(std::array<int, N> shape);
+
+    /// <summary> Creates an N dimensional Identity Matrix with shape `shape` </summary>
+    /// <param name=shape> Array containing the sizes of each dimension of the Matrix </param>
+    static HyperMatrix<N> Identity(std::array<int, N> shape);
+
+    /// <summary> Return the number of dimensions N this matrix has </summary>
     int GetDims();
+
+    /// <summary> returns value found at specified indices </summary>
+    /// <param name=indices> Array containing values of indices for the requested value </param>
+    double At(std::array<int, N> indices);
 
     operator std::string() const { return this->toString(); }
 };
@@ -40,6 +57,7 @@ template<unsigned int N>
 HyperMatrix<N>::HyperMatrix(std::array<int, N> shape)
 {
     this->shape = shape;
+    this->calculateStride();
 }
 
 template<unsigned int N>
@@ -47,6 +65,7 @@ HyperMatrix<N>::HyperMatrix(std::array<int, N> shape, std::vector<double> values
 {
     this->shape = shape;
     this->values = values;
+    this->calculateStride();
 }
 
 template<unsigned int N>
@@ -64,31 +83,102 @@ HyperMatrix<N> HyperMatrix<N>::Zeros(std::array<int, N> shape)
 }
 
 template<unsigned int N>
+HyperMatrix<N> HyperMatrix<N>::Ones(std::array<int, N> shape)
+{
+    int length = 1;
+    for (int i = 0; i < N; i++)
+        length *= shape[i];
+    std::vector<double> values(length, 1.0);
+    return HyperMatrix<N>(shape, values);
+}
+
+// TODO: Check for N dimensional iteration
+// https://stackoverflow.com/questions/14040260/how-to-iterate-over-n-dimensions
+template<unsigned int N>
+HyperMatrix<N> HyperMatrix<N>::Identity(std::array<int, N> shape)
+{
+    // Not implemented. Will fix later
+    throw;
+
+    int length = 1;
+    for (int i = 0; i < N; i++)
+        length *= shape[i];
+    std::vector<double> values(length, 0.0);
+
+
+}
+
+template<unsigned int N>
 int HyperMatrix<N>::GetDims()
 {
     return this->dims;
 }
 
-// TODO: Make this look better for higher dimensions
+template<unsigned int N>
+double HyperMatrix<N>::At(std::array<int, N> indices)
+{
+    int idx = this->convertIndex(indices);
+    return this->values[idx];
+}
+
+template<unsigned int N>
+void HyperMatrix<N>::calculateStride()
+{
+    // for (int k = 0; k < N; k++)
+    // {
+    //     strides[k] = 1;
+    //     for (int j = k+1; j < N-1; j++)
+    //         strides[k] *= this->shape[j];
+    // }
+
+    // for (int k = 0; k < N; k++) 
+    // {
+    //     strides[k] = 1;
+    //     for (int j = 0; j < k-1; j++)
+    //         strides[k] *= shape[j];
+    // }
+
+    this->strides[0] = 1;
+    for (int i = 1; i < N; i++)
+        strides[i] = shape[i-1]*strides[i-1];
+
+    for (int i = 0; i < N; i++)
+        std::cout << strides[i] << ',';
+    std::cout << std::endl;
+}
+
+template<unsigned int N>
+int HyperMatrix<N>::convertIndex(std::array<int, N> indices)
+{
+    int idx = 0;
+    for (int i = 0; i < N; i++)
+        idx += indices[i] * this->strides[i];
+    return idx;
+}
+
+// TODO: Try to print values similar to numpy style...or anything really?
 template<unsigned int N>
 std::string HyperMatrix<N>::toString() const
 {
     std::stringstream ss;
+    ss << "<HyperMatrix ";
 
-    ss << '\n';
+    ss << "shape=[";
+    for (int i = 0; i < N-1; i++)
+        ss << this->shape[i] << ',';
+    ss << this->shape[N-1] << ']';
 
-    int length = 1;
-    for (int i = 0; i < N; i++)
-        length *= this->shape[i];
+    // int length = 1;
+    // for (int i = 0; i < N; i++)
+    //     length *= this->shape[i];
 
-    for (int i = 0; i < length; i++)
-        if ( (i+1) % this->shape[N-1] == 0)
-            ss << this->values[i] << '\n';
-        else
-            ss << this->values[i] << ' ';
+    // for (int i = 0; i < length; i++)
+    //     if ( (i+1) % this->shape[N-1] == 0)
+    //         ss << this->values[i] << '\n';
+    //     else
+    //         ss << this->values[i] << ' ';
 
-    ss << '\n';
-
+    ss << '>';
     return ss.str();
 }
 
