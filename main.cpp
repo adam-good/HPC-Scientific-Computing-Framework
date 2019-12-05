b11661a95849403f6eb326e736435d48803478eb
Adam Good <dev.adamgood@gmail.com>
2019-12-05 16:31:10 -0600

    Changed testing

--- main.cpp
+++ main.cpp
@@ -1,103 +1,49 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <omp.h>
 #include "matrices.h"
+#include "matrices-omp.h"
 
 using namespace std;
 
-void TestApply()
-{
-    const unsigned int N = 2;
-
-    array<int, N> shape_A = {5,5};
-
-    int size_A = 1;
-    for (auto itr = shape_A.begin(); itr != shape_A.end(); itr++) size_A *= *itr;
-
-    vector<double> values_A(size_A);
-    for (int i = 0; i < size_A; i++) values_A[i] = i;
-
-    HyperMatrix<N> A(shape_A, values_A);
-
-    auto sigmoid = [](double x) { return 1.0 / (1.0 + exp(-x) ); };
-
-    cout << A << endl;
-    cout << A.Apply([](double x){ return sin(x);} ) << endl;
-}
-
-void TestArithmatic()
-{
-    const unsigned int N = 2;
-    array<int, N> shape_A = {3,2};
-    array<int, N> shape_B = {2,3};
-
-    int size = 1;
-    for (auto itr = shape_A.begin(); itr != shape_A.end(); itr++)
-        size *= *itr;
-
-
-    vector<double> values_A(size);
-    vector<double> values_B(size);
-    for (int i = 0; i < size; i++)
-    {
-        values_A[i] = i;
-        values_B[i] = size-i;    
-    }
-
-    HyperMatrix<N> A(shape_A, values_A);
-    HyperMatrix<N> B(shape_B, values_B);
-    HyperMatrix<N> C = A*B;
-    HyperMatrix<N> I = HyperMatrix<N>::Identity(C.getShape());
-    HyperMatrix<N> D = C + I;
-    HyperMatrix<N> M_0 = I - I;
-
-
-    cout << A << endl;
-    cout << endl;
-    cout << B << endl;
-    cout << endl;
-    cout << C << endl;
-    cout << endl;
-    cout << I << endl;
-    cout << endl;
-    cout << D << endl;
-    cout << endl;
-    cout << M_0 << endl;
-}
-
-void TestScale()
+void TestAdd()
 {
     const unsigned int N = 3;
-    int n = 300;//4096;
-    int m = 300;//2160;
-    int z = 3;
+    array<int, N> shape = {3, 300, 300};
 
-    array<int, N> shape_A = {z,n,m};
-    array<int, N> shape_B = {z,m,n};
+    HyperMatrix<N> A = HyperMatrix<N>::Zeros(shape);
+    HyperMatrix<N> B = HyperMatrix<N>::Ones(shape);
 
-    HyperMatrix<N> A = HyperMatrix<N>::Zeros(shape_A);
-    HyperMatrix<N> B = HyperMatrix<N>::Ones(shape_B);
+    A + B;
+}
 
-    A * B;
+void TestAdd_OMP()
+{
+    const unsigned int N = 3;
+    array<int, N> shape = {3, 300, 300};
 
+    HyperMatrix_OMP<N> A = HyperMatrix_OMP<N>::Zeros(shape);
+    HyperMatrix_OMP<N> B = HyperMatrix_OMP<N>::Ones(shape);
+
+    A + B;
 }
 
 void TimeTest(function<void()> test, string testName)
 {
-    clock_t begin = clock();
+    double begin = omp_get_wtime();
 
     test();
 
-    clock_t end = clock();
-    double elapsed_time = double(end - begin) / CLOCKS_PER_SEC;
+    double end = omp_get_wtime();
+    double elapsed_time = end - begin;
 
     cout << testName << "\n  Elapsed Time: " << elapsed_time << " Seconds" << endl;
 }
 
 int main(void)
 {
-    // TimeTest(TestArithmatic, "Arithmatic");
-    // TimeTest(TestApply, "Apply");
-    TimeTest(TestScale, "Scale");
+    TimeTest(TestAdd, "Serial Addition");
+    TimeTest(TestAdd_OMP, "OMP Addition");
     return 0;
 }
